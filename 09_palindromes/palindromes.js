45602921560da2dda7929643844c1f3a7a2739bc
susinoo <daniele.larosa12@gmail.com>
2022-08-29 17:42:02 +0200

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let reversed_word = ""
+    word = word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, '');
 
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversed_word += word[i]
+    }
+
+    word = word.split('')
+    reversed_word = reversed_word.split('')
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] != reversed_word[i]){
+            return false
+        }
+    }
+    return true
 };
 
+console.log(palindromes('Animal loots foliated detail of stool lamina'))
 // Do not edit below this line
 module.exports = palindromes;
