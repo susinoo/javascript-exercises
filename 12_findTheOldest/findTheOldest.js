c2b04a50ece8987555004c10dcdb30e26bfa1c0a
susinoo <daniele.larosa12@gmail.com>
2022-08-29 19:01:50 +0200

    Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let age = 0
+    let maxAge = 0
+    let thisYear = new Date().getFullYear();
+    let oldest = ""
 
+    array.forEach(element => {
+        if ('yearOfDeath' in element) {
+            age = element.yearOfDeath - element.yearOfBirth
+        }
+        else {
+            age = thisYear - element.yearOfBirth
+        }
+        if (age > maxAge) {
+            maxAge = age
+            oldest = element
+        }
+    });
+    return oldest
 };
 
 // Do not edit below this line
