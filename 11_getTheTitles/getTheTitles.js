9e65f8905cf0f2656302ba52a01d16c6dc08cedc
susinoo <daniele.larosa12@gmail.com>
2022-08-29 18:30:13 +0200

    Done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let arr = []
 
+    books.forEach(book => {
+        arr.push(book.title)
+    });
+
+    return arr
 };
 
 // Do not edit below this line
